54863ca1152116192b242d85441340db1fc1e212
MaridiM <maridim92@gmail.com>
2025-03-29 13:29:30 +0300

    Добавление нового компонента TasksHeader с мемоизацией и формой редактирования заголовка. Обновление экспорта в index.ts для упрощения доступа к компонентам задач и дашборда.

--- src/screens/Tasks.tsx
+++ src/screens/Tasks.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { MoreVertical, Plus } from 'lucide-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Columns3, MoreVertical, Plus, Rows3, Settings } from 'lucide-react'
 import { useTranslations } from 'next-intl'
+import { memo, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import { Header } from '@/widgets/Header'
-import { TasksHeader } from '@/widgets/tasks'
 
-import { Button } from '@/shared/components'
+import { Button, EditableTitle } from '@/shared/components'
+import { TEditableTitle, editableTitleSchema } from '@/shared/schemas'
 
 export default function Tasks() {
     const t = useTranslations('tasks')
@@ -129,3 +132,43 @@ export default function Tasks() {
         </>
     )
 }
+export const TasksHeader = memo(function TasksHeader() {
+    const t = useTranslations('tasks')
+
+    const [view, setView] = useState<'kanban' | 'list'>('kanban')
+
+    const form = useForm<TEditableTitle>({
+        resolver: zodResolver(editableTitleSchema),
+        defaultValues: {
+            title: ''
+        }
+    })
+
+    const { isDirty } = form.formState
+
+    return (
+        <header className='flex items-center justify-between'>
+            <div className='flex items-center gap-2'>
+                <Button
+                    variant='outline'
+                    size='icon'
+                    onClick={() => (view === 'kanban' ? setView('list') : setView('kanban'))}
+                >
+                    {view === 'kanban' && <Columns3 className='size-5 stroke-text stroke-[1.75px]' />}
+                    {view === 'list' && <Rows3 className='size-5 stroke-text stroke-[1.75px]' />}
+                </Button>
+
+                <EditableTitle initialTitle='Canban' form={form} isDirty={isDirty} />
+            </div>
+
+            <div className='flex items-center gap-2'>
+                <Button variant='outline' size='icon'>
+                    <Plus className='stroke-text' />
+                </Button>
+                <Button variant='outline' size='icon'>
+                    <Settings className='stroke-text' />
+                </Button>
+            </div>
+        </header>
+    )
+})
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,3 +1,4 @@
-export * from './dashboard'
 export * from './Header'
 export * from './Sidebar'
+export * from './dashboard'
+export * from './tasks'
