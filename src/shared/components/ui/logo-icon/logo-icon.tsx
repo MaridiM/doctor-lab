d6d4335ac59274ce274c2d3fe0baf0cae88b0bb4
MaridiM <maridim92@gmail.com>
2025-06-09 16:06:38 +0300

    Закомментированы импорты логотипов в компоненте LogoIcon и в файле индекса логотипов. Упрощен код, временно отключив использование логотипов для улучшения читаемости.

--- src/shared/components/ui/logo-icon/logo-icon.tsx
+++ src/shared/components/ui/logo-icon/logo-icon.tsx
@@ -2,28 +2,29 @@
 
 import { useTheme } from 'next-themes'
 
-import { DarkLogoFull, DarkLogoMini, DarkLogoText, LogoFull, LogoMini, LogoText } from '@/shared/assets/logo'
+// import { DarkLogoFull, DarkLogoMini, DarkLogoText, LogoFull, LogoMini, LogoText } from '@/shared/assets/logo'
 import { cn } from '@/shared/utils'
 
 import type { TIconProps } from './types'
 
 export function LogoIcon({ mini = true, onlyText = false, className, ...props }: TIconProps) {
-    const { theme } = useTheme()
-    const isDark = theme === 'dark'
+    // const { theme } = useTheme()
+    // const isDark = theme === 'dark'
 
-    const filterStyle = `drop-shadow(0px .5px 1px ${isDark ? '#1e1e1e' : '#999'})`
+    // const filterStyle = `drop-shadow(0px .5px 1px ${isDark ? '#1e1e1e' : '#999'})`
 
-    const commonProps = {
-        className: cn('h-10', className),
-        filter: filterStyle,
-        ...props
-    }
+    // const commonProps = {
+    //     className: cn('h-10', className),
+    //     filter: filterStyle,
+    //     ...props
+    // }
 
-    const Component = (() => {
-        if (onlyText) return isDark ? DarkLogoText : LogoText
-        if (mini) return isDark ? DarkLogoMini : LogoMini
-        return isDark ? DarkLogoFull : LogoFull
-    })()
+    // const Component = (() => {
+    //     if (onlyText) return isDark ? DarkLogoText : LogoText
+    //     if (mini) return isDark ? DarkLogoMini : LogoMini
+    //     return isDark ? DarkLogoFull : LogoFull
+    // })()
 
-    return <Component {...commonProps} />
+    // return <Component {...commonProps} />
+    return <span>DL</span>
 }
